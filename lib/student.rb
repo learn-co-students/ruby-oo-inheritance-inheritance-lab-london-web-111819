0ccf26bd1f93331165de7929c95e9cd4e149be82
badogan <basri.dogan.71@gmail.com>
2019-11-26 12:19:45 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
 
+    def initialize()
+        @knowledge =[]
+    end
+
+    def learn(var_str)
+        @knowledge << var_str
+    end
+
+    def knowledge
+        @knowledge
+    end
 end
